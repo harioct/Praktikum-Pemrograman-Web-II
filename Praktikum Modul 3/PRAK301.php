3868d7d70a9d3bd20e9c26c5019ca35352d639b0
harioct <114865767+harioct@users.noreply.github.com>
2024-05-01 20:45:04 +0800

    Add files via upload

---
+++ Praktikum Modul 3/PRAK301.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PRAK301</title>
+</head>
+<body>
+    <form action="" method="post">
+        Jumlah Peserta : <input type="number" name="peserta" min="1" value="<?=isset($_POST['peserta']) ? $_POST['peserta'] : ''?>">
+        <br>
+        <input type="submit" value="Cetak" name="cetak">
+    </form>
+    <?php
+        if(isset($_POST["cetak"])){
+            $i = 1;
+            $number = $_POST['peserta'];
+                while($i <= $number){
+                    if($i % 2 != 0){
+                        echo "<b><font color='red'>Peserta ke-$i</font></b>";
+                    }
+                    else{
+                        echo "<b><font color='green'>Peserta ke-$i</font></b>";
+                    }
+                    echo "<br><br>";
+                    $i++;
+                }
+        }
+    ?>
+</body>
+</html>
---
+++ Praktikum Modul 3/PRAK305.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PRAK305</title>
+</head>
+<body>
+    <form action="" method="post">
+        <input type="text" name="input">
+        <button type="submit" name="submit">submit</button>
+    </form>
+</body>
+</html>
+<?php
+if (isset($_POST["submit"])) {
+    $input = $_POST['input'];
+    echo "<h2>Input:</h2><br>$input<br>";
+    echo "<h2>Output:</h2><br>";
+
+    for ($i = 0; $i < strlen($input); $i++) {
+        $kapital = strtoupper($input);
+        echo $kapital[$i];
+        for ($j = 1; $j < strlen($input); $j++) {
+            $nonkapital = strtolower($input);
+            echo $nonkapital[$i];
+        }
+    }
+}
+?>
